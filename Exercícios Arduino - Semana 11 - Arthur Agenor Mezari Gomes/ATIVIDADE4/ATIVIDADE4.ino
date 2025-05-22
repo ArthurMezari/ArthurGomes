371e17d25036c3018fab7684b6f401551f78eff3
ArthurMezari <arthurmezari81@gmail.com>
2025-05-21 22:12:31 -0300

    Add files via upload

--- Exercícios Arduino - Semana 11 - Arthur Agenor Mezari Gomes/ATIVIDADE4/ATIVIDADE4.ino
+++ Exercícios Arduino - Semana 11 - Arthur Agenor Mezari Gomes/ATIVIDADE4/ATIVIDADE4.ino
@@ -1,31 +1,47 @@
-#define pin_led1 8     //o define nomeia o seu pino, logo depois você indica onde está conectado o seu cabo no arduino, no caso 8.
-#define pin_led2 9
-#define pin_led3 10
-#define pin_led4 11
+#define pin_led1 8    //definindo pino 8 como led1
+#define pin_led2 9    //definindo pino 9 como led2
+#define pin_led3 10   //definindo pino 10 como led3
 #define pin_botao1 2
 #define pin_botao2 3
 
 
-
 void setup() {
-   pinMode(pin_led1, OUTPUT); 
-  pinMode(pin_led2, OUTPUT);
-  pinMode(pin_led3, OUTPUT);
-  pinMode(pin_led4, OUTPUT);
-  pinMode(pin_botao1, INPUT);
-  pinMode(pin_botao2, INPUT);
-
+  Serial.begin(9600);          //define a velocidade da comunicação
+  pinMode (pin_led1, OUTPUT);  //definindo led1 saida
+  pinMode (pin_led2, OUTPUT);  //definindo led2 saida
+  pinMode (pin_led3, OUTPUT);  //definindo led3 saida
+  pinMode (pin_botao1, INPUT);
+  pinMode (pin_botao2, INPUT);
 }
 
 void loop() {
-    bool estado = digitalRead(pin_botao1);
-  if (estado == 1) {
-    digitalWrite(pin_led1, HIGH);
-    digitalWrite(pin_led2)
+  bool estado1 = digitalRead(pin_botao1);
+  bool estado2 = digitalRead(pin_botao2);
+  if (estado1 == 1 && estado2 == 1) {
+    digitalWrite (pin_led1, HIGH);
+  }
+  else {
+    digitalWrite (pin_led1, LOW);
+  }
 
+  if (estado1 == 1 ||estado2 == 1){
+    digitalWrite (pin_led2, HIGH);
+  }
+  else {
+    digitalWrite (pin_led2, LOW);
+  }
 
-  
-
-  
-
+    if (estado1 != estado2){
+    digitalWrite (pin_led3, HIGH);
+  }
+  else {
+    digitalWrite (pin_led3, LOW);
+  }
+    
 }
+
+  
+
+  
+
+
