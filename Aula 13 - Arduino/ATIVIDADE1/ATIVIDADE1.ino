1669d856495f4b48dd6403407f17b53c96e8d145
ArthurMezari <arthurmezari81@gmail.com>
2025-06-16 14:03:57 -0300

    Aula 13 - Arduino

---
+++ Aula 13 - Arduino/ATIVIDADE1/ATIVIDADE1.ino
@@ -0,0 +1,90 @@
+#define pin_led1 8     //o define nomeia o seu pino, logo depois você indica onde está conectado o seu cabo no arduino, no caso 8.
+#define pin_led2 9
+#define pin_led3 10
+#define pin_led4 11
+
+
+
+
+void setup() {
+  Serial.begin(9600);  //velocidade em que você se comunica com o aruduino
+  pinMode(pin_led1, OUTPUT); 
+  pinMode(pin_led2, OUTPUT);
+  pinMode(pin_led3, OUTPUT);
+  pinMode(pin_led4, OUTPUT);
+
+}
+void loop() {
+  if (Serial.available() == 1 ) {
+    char i = Serial.read();
+    Serial.println(i);
+
+    if(i == '1') {
+      digitalWrite(pin_led1, HIGH);
+    }
+
+    if(i == '2') {
+      digitalWrite(pin_led2, HIGH);
+    }
+    
+    if(i == '3') {
+      digitalWrite(pin_led3, HIGH);
+    }
+
+    if(i == '4') {
+      digitalWrite(pin_led4, HIGH);
+    }
+
+    if(i == '5') {
+      digitalWrite(pin_led1, LOW);
+    }
+
+    if(i == '6') {
+      digitalWrite(pin_led2, LOW);
+    }
+
+    if(i == '7') {
+      digitalWrite(pin_led3, LOW);
+    }
+
+    if(i == '8') {
+      digitalWrite(pin_led4, LOW);
+    }
+
+    if(i == '9') {
+      digitalWrite(pin_led1, HIGH);
+      digitalWrite(pin_led2, HIGH);
+      digitalWrite(pin_led3, HIGH);
+      digitalWrite(pin_led4, HIGH);
+    }
+
+    if(i == '0') {
+      digitalWrite(pin_led1, LOW);
+      digitalWrite(pin_led2, LOW);
+      digitalWrite(pin_led3, LOW);
+      digitalWrite(pin_led4, LOW);
+    }
+
+    }
+}
+  
+  
+
+  
+  
+  
+  /*
+  Serial.println("Liga led");
+  digitalWrite(pin_led1, HIGH);
+  digitalWrite(pin_led2, HIGH);
+  digitalWrite(pin_led3, HIGH);
+  digitalWrite(pin_led4, HIGH);
+  delay(1000);
+  Serial.println("Desliga led");
+  digitalWrite(pin_led1, LOW);
+  digitalWrite(pin_led2, LOW);
+  digitalWrite(pin_led3, LOW);
+  digitalWrite(pin_led4, LOW);
+  delay(1000);
+}
+  */
